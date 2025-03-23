adcfe63ecd1b3693be677dabe00e143213df9282
krzysiek641886 <krzysiek6400@gmail.com>
2025-03-23 17:27:00 +0100

    A skeleton for periodically processing orders

--- web_server_with_database/src/api/mod.rs
+++ web_server_with_database/src/api/mod.rs
@@ -1,5 +1,6 @@
 mod api;
 pub use api::app_init_status_handler;
-pub use api::initialize_api_handler;
 pub use api::form_submission_handler;
 pub use api::get_orders_handler;
+pub use api::initialize_api_handler;
+pub use api::send_result_to_client;
--- web_server_with_database/src/main.rs
+++ web_server_with_database/src/main.rs
@@ -2,14 +2,20 @@ use actix_files as fs;
 use actix_web::{web, App, HttpServer};
 use clap::Parser;
 mod database_handler;
-use database_handler::initialize_db;
-mod orca_slicer_interface;
+use database_handler::{
+    add_evaluation_to_db, get_pending_order, initialize_db, remove_order_from_db,
+};
 use lazy_static::lazy_static;
-use orca_slicer_interface::initialize_orca_slicer_if;
+mod orca_slicer_interface;
+use orca_slicer_interface::{
+    get_orca_slicer_evaluation, initialize_orca_slicer_if, EvaluationResult,
+};
 use std::sync::Mutex;
+use tokio::time::{interval, Duration};
 mod api;
 use api::{
     app_init_status_handler, form_submission_handler, get_orders_handler, initialize_api_handler,
+    send_result_to_client,
 };
 
 /// Command-line arguments
@@ -54,10 +60,33 @@ fn initialize_modules_with_cmd_arguments(args: Args) {
     initialize_api_handler(true);
 }
 
+async fn process_orders_periodically(interval_seconds: u64) {
+    let interval_duration = Duration::from_secs(interval_seconds); // Check every 60 seconds
+    let mut interval: tokio::time::Interval = interval(interval_duration);
+
+    loop {
+        println!("Checking for pending orders...");
+        match get_pending_order() {
+            Some(order) => {
+                let slicer_evaluation_result: EvaluationResult = get_orca_slicer_evaluation(&order);
+                send_result_to_client(&slicer_evaluation_result);
+                add_evaluation_to_db(slicer_evaluation_result);
+                remove_order_from_db(order);
+            }
+            None => {
+                interval.tick().await;
+            }
+        }
+    }
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let args = Args::parse();
     initialize_modules_with_cmd_arguments(args);
+
+    // Spawn the background task for processing orders
+    tokio::spawn(process_orders_periodically(60));
 
     println!("Starting server at http://127.0.0.1:8080");
     HttpServer::new(|| {
