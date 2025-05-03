2ee01301abe1319a07cd144687e11351a5226d04
krzysiek641886 <krzysiek6400@gmail.com>
2025-05-03 14:59:42 +0200

    Reworked prusa_slicer_interface.rs to allow for mock usage

--- web_server_with_database/src/common_utils/global_traits.rs
+++ web_server_with_database/src/common_utils/global_traits.rs
@@ -1,7 +1,7 @@
 use crate::common_utils::global_types::{SubmittedOrderData, EvaluationResult};
 use std::io::{self};
 
-pub trait SlicerInterfaceImpl {
+pub trait SlicerInterfaceImpl: Send + Sync {
     fn ping(&self, prusa_path: &str) -> io::Result<()>;
     fn evaluate(&self, order: &SubmittedOrderData, slicer_path: &str, ws_path: &str) -> EvaluationResult;
 }
--- web_server_with_database/src/prusa_slicer_interface/prusa_slicer_interface.rs
+++ web_server_with_database/src/prusa_slicer_interface/prusa_slicer_interface.rs
@@ -1,83 +1,92 @@
 /* IMPORTS FROM LIBRARIES */
 use lazy_static::lazy_static;
+use std::io;
 use std::sync::Mutex;
 
 /* IMPORTS FROM OTHER MODULES */
+use crate::common_utils::global_traits::SlicerInterfaceImpl;
 use crate::common_utils::global_types::{EvaluationResult, SubmittedOrderData};
 use crate::prusa_slicer_interface::prusa_slicer_cli::PrusaSlicerCli;
-use crate::common_utils::global_traits::SlicerInterfaceImpl;
 
 /* PRIVATE TYPES AND VARIABLES */
 struct State {
     ws_path: Mutex<Option<String>>,
     slicer_exec_path: Mutex<Option<String>>,
-    slicer_interface: Mutex<Box<SlicerInterfaceImpl>>,
+    slicer_interface: Mutex<Box<dyn SlicerInterfaceImpl>>,
 }
 
-
-static SLICER_IF_STATE: State = State {
-    ws_path: Mutex::new(None),
-    slicer_exec_path: Mutex::new(None),
-    slicer_interface: Mutex::new(Box::new(None)),
-};
-
-/* PUBLIC TYPES AND VARIABLES */
+lazy_static! {
+    static ref SLICER_IF_STATE: State = State {
+        ws_path: Mutex::new(None),
+        slicer_exec_path: Mutex::new(None),
+        slicer_interface: Mutex::new(Box::new(PrusaSlicerCli {})),
+    };
+}
 
 /* PRIVATE FUNCTIONS */
-
-/* PUBLIC FUNCTIONS */
-
-/**
- * @brief Initializes the Prusa Slicer interface.
- *
- * This function sets up the Prusa Slicer interface by updating the global state
- * with the workspace path and slicer executable path. It also pings the Prusa Slicer
- * to ensure it is reachable.
- *
- * @param ws_path Path to the workspace directory.
- * @param prusa_path Path to the Prusa Slicer executable.
- */
-pub fn initialize_prusa_slicer_if(ws_path: &str, prusa_path: &str) {
+fn setup_module_state(
+    ws_path: &str,
+    prusa_path: &str,
+) -> io::Result<()> {
     let mut ws_path_lock = SLICER_IF_STATE.ws_path.lock().unwrap();
     let mut slicer_exec_path_lock = SLICER_IF_STATE.slicer_exec_path.lock().unwrap();
+
+    *ws_path_lock = Some(ws_path.to_string());
+    *slicer_exec_path_lock = Some(prusa_path.to_string());
+
+    Ok(())
+}
+
+/* PUBLIC FUNCTIONS */
+pub fn initialize_prusa_slicer_if(ws_path: &str, prusa_path: &str) {
+    if let Err(e) = setup_module_state(ws_path, prusa_path) {
+        panic!("Failed to setup Prusa Slicer Interface: {:?}", e);
+    }
+
     let slicer_interface_lock = SLICER_IF_STATE.slicer_interface.lock().unwrap();
-
-    *ws_path_lock = ws_path.to_string();
-    *slicer_exec_path_lock = prusa_path.to_string();
-
-    if let Err(e) = *slicer_interface_lock.ping(prusa_path) {
+    if let Err(e) = slicer_interface_lock.ping(prusa_path) {
         panic!("Failed to ping Prusa Slicer: {:?}", e);
     }
 }
 
-/**
- * @brief Evaluates an order using the Prusa Slicer.
- *
- * This function takes a submitted order and returns an evaluation result.
- * Currently, it returns a placeholder result.
- *
- * @param _order Reference to the submitted order data.
- * @return EvaluationResult The result of the evaluation.
- */
 pub fn get_prusa_slicer_evaluation(order: &SubmittedOrderData) -> EvaluationResult {
     let prusa_path = &*SLICER_IF_STATE.slicer_exec_path.lock().unwrap();
     let workspace_path = &*SLICER_IF_STATE.ws_path.lock().unwrap();
     let slicer_interface_lock = SLICER_IF_STATE.slicer_interface.lock().unwrap();
-    return *slicer_interface_lock.evaluate(order, prusa_path, workspace_path);
+    slicer_interface_lock.evaluate(
+        order,
+        prusa_path.as_deref().unwrap(),
+        workspace_path.as_deref().unwrap(),
+    )
 }
 
+#[cfg(test)]
 mod tests {
-    // use super::*;
+    use super::*;
+    use crate::prusa_slicer_interface::prusa_slicer_mock::PrusaSlicerMock;
 
     #[test]
     fn test_initialize_prusa_slicer_if() {
-        //TBA
-        // initialize_prusa_slicer_if("new_ws_path", "new_prusa_path");
+        println!("FOO begin");
 
-        // let ws_path_lock = SLICER_IF_STATE.ws_path.lock().unwrap();
-        // let slicer_exec_path_lock = SLICER_IF_STATE.slicer_exec_path.lock().unwrap();
 
-        // assert_eq!(*ws_path_lock, "new_ws_path");
-        // assert_eq!(*slicer_exec_path_lock, "new_prusa_path");
+        {
+            let mut slicer_interface_lock = SLICER_IF_STATE.slicer_interface.lock().unwrap();
+            let mocked_interface = Box::new(PrusaSlicerMock {
+                price_to_return: 42.0,
+                ping_result: true,
+            });
+            *slicer_interface_lock = mocked_interface;
+        }
+
+        let ws_path = "foobar";
+        let prusa_path = "foobar";
+        assert!(setup_module_state(ws_path, prusa_path).is_ok());
+
+        {
+            let slicer_interface_lock = SLICER_IF_STATE.slicer_interface.lock().unwrap();
+            assert!(slicer_interface_lock.ping(prusa_path).is_ok());
+        }
+        println!("FOO1");
     }
 }
