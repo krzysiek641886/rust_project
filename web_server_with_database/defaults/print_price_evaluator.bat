5d2ba6bc5f0469dad62016d9959af460a8965e36
krzysiek641886 <krzysiek6400@gmail.com>
2025-08-09 15:23:35 +0200

    Added a README.md file to release package

--- web_server_with_database/defaults/print_price_evaluator.bat
+++ web_server_with_database/defaults/print_price_evaluator.bat
@@ -6,5 +6,6 @@ set price_calculator_params=data_files\price_calculator_params.json
 set current_directory=%cd%
 @echo on
 
-.\web_server_with_database.exe --ws-path "%current_directory%" --db-name data_files\price_evaluator_database.db --prusa-slicer-path "%prusa_slicer_path%" --system windows --price-params "%price_calculator_params%"
+start /B .\web_server_with_database.exe --ws-path "%current_directory%" --db-name data_files\price_evaluator_database.db --prusa-slicer-path "%prusa_slicer_path%" --system windows --price-params "%price_calculator_params%"
+start http://127.0.0.1:8080/
 pause
